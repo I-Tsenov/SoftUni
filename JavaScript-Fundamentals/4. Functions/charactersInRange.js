04461f9c06fb4922c7607219ff1dff08b21f508d
I.Tsenov <tobetsenov@gmail.com>
2021-06-02 19:18:14 +0300

    solve charactersInRange

---
+++ JavaScript-Fundamentals/4. Functions/charactersInRange.js
@@ -0,0 +1,21 @@
+function charactersInRange(char1, char2) {
+
+    let inRange = ""
+
+    if (char1.charCodeAt() > char2.charCodeAt()) {
+        for (var i = char2.charCodeAt() + 1; i < char1.charCodeAt(); i++) {
+            inRange += String.fromCharCode(i) + " ";
+        }
+    } else {
+        for (var i = char1.charCodeAt() + 1; i < char2.charCodeAt(); i++) {
+            inRange += String.fromCharCode(i) + " ";
+        }
+    }
+
+    return inRange;
+    
+}
+
+console.log(charactersInRange("a", "d"));
+console.log(charactersInRange("#", ":"));
+console.log(charactersInRange("C", "#"));
