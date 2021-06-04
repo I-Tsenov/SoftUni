71bd0044cb93db588dc09d00c057612d85d2f827
I.Tsenov <tobetsenov@gmail.com>
2021-06-04 16:31:59 +0300

    solve loadingBar

---
+++ JavaScript-Fundamentals/4. Functions/loadingBar.js
@@ -0,0 +1,31 @@
+function loadingBar(num) {
+
+    let bar = `[..........]`;
+    let loadingBar = "";
+    let result = ``;
+    let load = num / 10;
+    let loadPercentage = load * 10;
+
+    if (num % 10 === 0) {
+
+        for (let i = 0; i < bar.length; i++) {
+
+            if (i <= load) {
+                loadingBar += bar[i].replace(".", "%")
+            } else {
+                loadingBar += bar[i];
+            }
+        }
+    }
+
+    if (loadPercentage === 100) {
+        result = `100% Complete! \n${loadingBar}`;
+    } else {
+        result = `${loadPercentage}% ${loadingBar} \nStill loading...`
+    }
+
+    return result;
+
+}
+
+console.log(loadingBar(50));
