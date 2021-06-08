5700462d9a6fc69ba58c549c6b7629c7d2614be9
I.Tsenov <tobetsenov@gmail.com>
2021-06-08 22:16:00 +0300

    solve perfectNumber

---
+++ JavaScript-Fundamentals/4. Functions/perfectNumber.js
@@ -0,0 +1,22 @@
+function perfectNumber(num) {
+
+    let sum = 0;
+    let result = "It's not so perfect.";
+
+    for (let i = 1; i <= num / 2; i++) {
+
+        if (num % i === 0) {
+            sum += i;
+        }
+
+        if (sum === num) {
+            result = "We have a perfect number!";
+            break;
+        }
+    }
+    
+    return result;
+
+}
+
+console.log(perfectNumber(28));
