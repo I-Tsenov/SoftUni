b893a1db565bcdd2770a8674865a66078c58caef
I.Tsenov <tobetsenov@gmail.com>
2021-09-20 18:45:20 +0300

    solve arrayWithDelimiter

---
+++ JavaScript-Advanced/JS advanced/2. Arrays and Nested Arrays/arrayWithDelimiter.js
@@ -0,0 +1,16 @@
+function arrayWithDelimiter (arr, delimiter) {
+
+    let output = arr.join(delimiter)
+
+    return output
+
+
+}
+
+console.log(arrayWithDelimiter(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+))
