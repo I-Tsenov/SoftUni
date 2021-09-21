bcc7db1d39d7dd8e64b020c759e32b3106e683ce
I.Tsenov <tobetsenov@gmail.com>
2021-09-21 14:25:44 +0300

    solve rotateArray

---
+++ JavaScript-Advanced/JS advanced/2. Arrays and Nested Arrays/rotateArray.js
@@ -0,0 +1,24 @@
+function rotateArray (arr, num) {
+
+    for (let i = 0; i < num; i++) {
+        let temp = arr.pop();
+        arr.unshift(temp)
+    }
+
+    return arr.join(' ')
+
+}
+
+console.log(rotateArray(['1', 
+'2', 
+'3', 
+'4'], 
+2
+))
+
+console.log(rotateArray(['Banana', 
+'Orange', 
+'Coconut', 
+'Apple'], 
+15
+))
