087b53d285638c21c3098094df323856571fd50f
I.Tsenov <tobetsenov@gmail.com>
2021-09-23 11:05:37 +0300

    solve fruit

---
+++ JavaScript-Advanced/JS advanced/1. Syntax, Functions and Statements/fruit.js
@@ -0,0 +1,10 @@
+function fruit(fruit, weightGrams, price) {
+
+    let weightKg = weightGrams / 1000;
+    let money = weightKg * price
+
+console.log(`I need $${money.toFixed(2)} to buy ${weightKg.toFixed(2)} kilograms ${fruit}.`)
+
+}
+
+fruit('orange', 2500, 1.80)
