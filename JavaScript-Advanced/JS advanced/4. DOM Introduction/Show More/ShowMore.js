4aa4070731dbd0fd2de536f48b832e3089eaab01
I.Tsenov <tobetsenov@gmail.com>
2021-10-08 10:49:12 +0300

    solve ShowMore

---
+++ JavaScript-Advanced/JS advanced/4. DOM Introduction/Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const additionalText = document.getElementById('text');
+    additionalText.style.display = 'inline';
+    const hrefLink = document.getElementById('more')
+    hrefLink.style.display = 'none'
+} 
